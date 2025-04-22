b667cf499803ada26791f1de5a4b6415e01c914c
Shounak <shounak2202@gmail.com>
2025-04-21 20:31:53 -0400

    Updated sentiment analysis script to include new stock news

--- stock-analysis-project.py
+++ stock-analysis-project.py
@@ -56,5 +56,52 @@ conn.close()
 # In[ ]:
 
 
+df = pd.read_csv('stock.csv', na_values=['null'], index_col='date', parse_dates=True, infer_datetime_format=True)
+df.isna().sum()
+df.describe().T
+df.corr()
 
+
+# In[ ]:
+
+
+df1 = df
+df1['date'] = pd.to_datetime(df1.index)
+stock_data = df1.set_index('date')
+close_px = stock_data['adj_close']
+open_px = stock_data['open']
+high_px = stock_data['high']
+low_px = stock_data['low']
+plt.ylabel("prices")
+plt.xlabel("Date")
+plt.title(f"{company_name} Adjusted Close, Open, High, and Low Price over Time")
+plt.plot(stock_data['adj_close'], label='Adjusted Closing Price', color='green')
+plt.plot(stock_data['open'], label='Open', color='red')
+plt.plot(stock_data['high'], label='High', color='yellow')
+plt.plot(stock_data['low'], label='Low', color='cyan')
+plt.legend(loc=2)
+plt.show()
+
+
+# In[ ]:
+
+
+import nltk
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+response = requests.get("https://finance.yahoo.com/quote/" + company_name)
+html_source = str(response.content)
+re_comments = '<p class=".*?">(.*?).<'
+comments_list = re.findall(re_comments, html_source)
+output_file = open("comments.txt", "w")
+for comment in comments_list:
+    print(comment, file=output_file)
+output_file.close()
+
+text = open("comments.txt", "r").read()
+wc = WordCloud().generate(text)
+plt.imshow(wc)
+plt.axis("off")
+plt.show()
 
